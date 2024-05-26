cb8dcfc5570becb37c9c58e2ff6fc7faed887b81
djaciel <dj_roses@hotmail.com>
2024-05-26 17:06:54 -0600

    add random delay before scheduling cron job for liquidation checker

--- packages/liquidation-checker/src/index.ts
+++ packages/liquidation-checker/src/index.ts
@@ -1,8 +1,13 @@
 import cron from 'node-cron'
 import { liquidationChecker } from './liquidatorCheckerHandler'
 
-// Schedule the cron job to run every 30 seconds
-cron.schedule('*/30 * * * * *', liquidationChecker)
+// generate a random delay between 0 and 30 seconds
+const randomDelay = Math.floor(Math.random() * 30000)
+
+setTimeout(() => {
+  cron.schedule('*/30 * * * * *', liquidationChecker)
+  liquidationChecker()
+}, randomDelay)
 
 // run liquidationChecker() once to fetch events for all markets
 // liquidationChecker()
