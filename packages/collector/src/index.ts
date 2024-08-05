5d66320e84a65ed0191e9dac4f64d20ebd4e7150
djaciel <dj_roses@hotmail.com>
2024-08-05 17:14:53 -0600

    update index collector file

--- packages/collector/src/index.ts
+++ packages/collector/src/index.ts
@@ -1,8 +1,40 @@
 import cron from 'node-cron'
 import { fetchAndProcessEventsForAllMarkets } from './marketEventHandler'
+import { Networks } from './constants'
+import chalk from 'chalk'
+import dotenv from 'dotenv'
 
-// Schedule the cron job to run every 2 minutes
-cron.schedule('*/2 * * * *', fetchAndProcessEventsForAllMarkets)
+dotenv.config()
 
-// run fetchAndProcessEventsForAllMarkets() once to fetch events for all markets
-// fetchAndProcessEventsForAllMarkets()
+const log = console.log
+
+let taskRunning = false
+
+async function start() {
+  if (taskRunning) {
+    log(chalk.bold.red('Task is already running. Skipping this run...'))
+    return
+  }
+
+  taskRunning = true
+
+  for (const network of Object.values(Networks)) {
+    try {
+      await fetchAndProcessEventsForAllMarkets(network)
+    } catch (error) {
+      log(chalk.bold.red(`Error fetching events for network: ${network} `))
+      console.error(error)
+    }
+  }
+
+  taskRunning = false
+}
+
+if (process.env.NODE_ENV === 'dev') {
+  log(chalk.bold.green('Running in dev mode'))
+  start()
+} else {
+  log(chalk.bold.green('Running in prod mode'))
+  // Schedule the cron job to run every 2 minutes
+  cron.schedule('*/2 * * * *', start)
+}
