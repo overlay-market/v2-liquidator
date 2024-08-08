d5405e545a2a65e3068b89de0dd7fa1a4a6ad42b
djaciel <dj_roses@hotmail.com>
2024-08-07 19:28:33 -0600

    udpate index liquidation-checker file

--- packages/liquidation-checker/src/index.ts
+++ packages/liquidation-checker/src/index.ts
@@ -1,25 +1,58 @@
 import cron from 'node-cron'
 import { liquidationChecker } from './liquidatorCheckerHandler'
-import dotenv from 'dotenv';
-import { config } from './config';
+import dotenv from 'dotenv'
+import { config } from './config'
+import chalk from 'chalk'
 
-dotenv.config();
+dotenv.config()
 
-const cronSchedule = config[process.env.MARKET || 'default'].cron_schedule;
+let taskRunning = false
 
-cron.schedule(cronSchedule, async () => {
+const log = console.log
+
+async function start() {
+  if (!process.env.MARKET || !config[process.env.MARKET]) {
+    log(chalk.bold.red('MARKET must be provided'))
+    return
+  }
+  if (taskRunning) {
+    log(chalk.bold.red('Task is already running. Skipping this run...'))
+    return
+  }
+
+  taskRunning = true
+
   try {
-    await liquidationChecker();
+    await liquidationChecker()
+  } catch (error) {
+    log(chalk.bold.red(`Error running liquidation checker for market: ${process.env.MARKET}`))
+    console.error(error)
+  } finally {
+    taskRunning = false
+  }
+}
+
+function getSchedule() {
+  if (!process.env.MARKET || !config[process.env.MARKET]) {
+    log(chalk.bold.red('MARKET must be provided'))
+    process.exit(1)
+  }
+  return config[process.env.MARKET].cron_schedule
+}
+
+if (process.env.NODE_ENV === 'dev') {
+  log(chalk.bold.green('Running in dev mode'))
+  start()
+} else {
+  log(chalk.bold.green('Running in prod mode'))
+  // Schedule the cron job to run every 2 minutes
+  cron.schedule(getSchedule(), async () => {
+    await start()
     // free up memory with garbage collection
     if (global.gc) {
-      global.gc();
+      global.gc()
     } else {
-      console.warn('Garbage collection is not exposed');
+      console.warn('Garbage collection is not exposed')
     }
-  } catch (error) {
-    console.error('Error in liquidationChecker:', error);
-  }
-});
-
-// run liquidationChecker() once to fetch events for all markets
-// liquidationChecker()
+  })
+}
