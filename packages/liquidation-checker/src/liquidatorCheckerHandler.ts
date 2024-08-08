1d9c8218e80db77311c5b6415b0002eb52e3ed87
djaciel <dj_roses@hotmail.com>
2024-08-07 19:29:48 -0600

    update liquidation-checker core to implement multichain

--- packages/liquidation-checker/src/liquidatorCheckerHandler.ts
+++ packages/liquidation-checker/src/liquidatorCheckerHandler.ts
@@ -1,69 +1,47 @@
 import { Worker } from 'worker_threads'
-import { markets } from './constants'
 import dotenv from 'dotenv'
 import chalk from 'chalk'
-import { selectRpc } from './rpcHandler'
 import redis from './redisHandler'
-import { config } from './config'
+import { config, MarketConfig } from './config'
+import { Networks, Position } from './constants'
 
 dotenv.config()
 
 const MAX_RETRIES = 3 // maximum number of retries for each worker
 const WORKER_TIMEOUT_MS = 15000
-const MULTICALL_BATCH_SIZE = config[process.env.MARKET || 'default'].multicall_batch_size
-const POSITIONS_PER_RUN = config[process.env.MARKET || 'default'].positions_per_run
-const WORKERS_AMOUNT = config[process.env.MARKET || 'default'].workers
 
 const log = console.log
 
-let taskRunning = false
-
-interface Position {
-  positionId: string
-  owner: string
-}
-
 // create a promise for each worker
 async function createWorkerPromise(
-  marketAddress: string,
-  positions: Position[],
+  market: MarketConfig,
+  positions: Partial<Record<Networks, Position[]>>,
   workerIndex: number,
-  rpcUrls: string[],
   retryCount: number = 0
 ): Promise<number> {
-  const rpcUrl = await selectRpc(rpcUrls)
-
-  if (!rpcUrl) {
-    const error = new Error('No healthy RPC found')
-    log(chalk.red(`Error: ${error.message}`))
-    throw error
-  }
-
   return new Promise((resolve, reject) => {
     const worker = new Worker('./dist/worker.js', {
       workerData: {
+        market,
         positions,
-        marketAddress,
-        batchSize: MULTICALL_BATCH_SIZE,
-        rpcUrl,
       },
     })
 
-    const timeoutId = setTimeout(() => {
-      worker.terminate()
-      const timeoutError = new Error(`Worker timeout: ${workerIndex} at RPC ${rpcUrl}`)
-      log(chalk.red(`Error: ${timeoutError.message}`))
-      reject(timeoutError)
-    }, WORKER_TIMEOUT_MS)
+    // const timeoutId = setTimeout(() => {
+    //   worker.terminate()
+    //   const timeoutError = new Error(`Worker timeout: ${workerIndex}`)
+    //   log(chalk.red(`Error: ${timeoutError.message}`))
+    //   reject(timeoutError)
+    // }, WORKER_TIMEOUT_MS)
 
     worker.on('message', (result: number) => {
-      clearTimeout(timeoutId)
+      // clearTimeout(timeoutId)
       log(chalk.bgBlue(`Worker ${workerIndex} completed with results:`, result))
       resolve(result)
     })
 
     worker.on('error', (error) => {
-      clearTimeout(timeoutId)
+      // clearTimeout(timeoutId)
       log(chalk.red(`Worker ${workerIndex} error: ${error}`))
       if (retryCount < MAX_RETRIES) {
         log(
@@ -72,9 +50,7 @@ async function createWorkerPromise(
           )
         )
         setTimeout(() => {
-          resolve(
-            createWorkerPromise(marketAddress, positions, workerIndex, rpcUrls, retryCount + 1)
-          )
+          resolve(createWorkerPromise(market, positions, workerIndex, retryCount + 1))
         }, 1000 * Math.pow(2, retryCount)) // Exponential backoff
       } else {
         reject(new Error(`Worker ${workerIndex} failed after ${MAX_RETRIES} retries`))
@@ -82,7 +58,7 @@ async function createWorkerPromise(
     })
 
     worker.on('exit', (code) => {
-      clearTimeout(timeoutId)
+      // clearTimeout(timeoutId)
       worker.unref()
       if (code !== 0) {
         const exitError = new Error(`Worker ${workerIndex} stopped with exit code ${code}`)
@@ -94,9 +70,7 @@ async function createWorkerPromise(
             )
           )
           setTimeout(() => {
-            resolve(
-              createWorkerPromise(marketAddress, positions, workerIndex, rpcUrls, retryCount + 1)
-            )
+            resolve(createWorkerPromise(market, positions, workerIndex, retryCount + 1))
           }, 1000 * Math.pow(2, retryCount)) // Exponential backoff
         } else {
           reject(new Error(`Worker ${workerIndex} failed after ${MAX_RETRIES} retries`))
@@ -107,22 +81,36 @@ async function createWorkerPromise(
 }
 
 // distribute work to workers
-async function distributeWorkToWorkers(marketAddress: string, positions: Position[]) {
-  const rpcUrls = process.env.RPC_URLS?.split(',') ?? []
-  if (rpcUrls.length === 0) {
-    throw new Error('At least one RPC_URLS must be provided')
-  }
-
-  // distribute positions to workers
-  const positionsPerWorker = Math.ceil(positions.length / WORKERS_AMOUNT)
+async function distributeWorkToWorkers(
+  market: MarketConfig,
+  positions: Partial<Record<Networks, Position[]>>
+) {
   let workerPromises: Promise<unknown>[] = []
 
-  for (let i = 0; i < WORKERS_AMOUNT; i++) {
-    const startPos = i * positionsPerWorker
-    const endPos = Math.min(startPos + positionsPerWorker, positions.length)
-    workerPromises.push(
-      createWorkerPromise(marketAddress, positions.slice(startPos, endPos), i, rpcUrls)
-    )
+  if (market.workers === 1) {
+    workerPromises.push(createWorkerPromise(market, positions, 0))
+  } else {
+    // distribute positions to workers
+    const positionsPerWorkerPerNetwork: Partial<Record<Networks, number>> = {}
+
+    for (const network of Object.keys(positions) as Networks[]) {
+      const tempPositions = positions[network]
+      if (!tempPositions) continue
+      positionsPerWorkerPerNetwork[network] = Math.ceil(tempPositions.length / market.workers)
+    }
+
+    for (let i = 0; i < market.workers; i++) {
+      const tempPositions: Partial<Record<Networks, Position[]>> = {}
+      for (const network of Object.keys(positions) as Networks[]) {
+        const positionsPerWorker = positionsPerWorkerPerNetwork[network]
+        const networkPositions = positions[network]
+        if (!networkPositions || !positionsPerWorkerPerNetwork[network] || !positionsPerWorker) continue
+        const startPos = i * positionsPerWorker
+        const endPos = Math.min(startPos + positionsPerWorker, networkPositions.length)
+        tempPositions[network] = networkPositions.slice(startPos, endPos)
+      }
+      workerPromises.push(createWorkerPromise(market, tempPositions, i))
+    }
   }
 
   const results = await Promise.allSettled(workerPromises)
@@ -137,14 +125,15 @@ async function distributeWorkToWorkers(marketAddress: string, positions: Positio
 }
 
 // fetch positions from redis
-async function fetchPositions(
+async function fetchPositionsByNetwork(
   marketAddress: string,
+  network: Networks,
   currentIndex: number,
   endIndex: number
 ): Promise<Position[]> {
   const pipeline = redis.pipeline()
 
-  pipeline.zrange(`position_index:${marketAddress}`, currentIndex, endIndex)
+  pipeline.zrange(`position_index:${network}:${marketAddress}`, currentIndex, endIndex)
   const results = await pipeline.exec()
   if (!results || results.length === 0 || results[0][1] === null) {
     return []
@@ -155,7 +144,7 @@ async function fetchPositions(
   }
 
   const ownerPipeline = redis.pipeline()
-  ownerPipeline.hmget(`positions:${marketAddress}`, ...positionIds)
+  ownerPipeline.hmget(`positions:${network}:${marketAddress}`, ...positionIds)
   const ownerResults = await ownerPipeline.exec()
   if (!ownerResults || ownerResults.length === 0 || ownerResults[0][1] === null) {
     return []
@@ -165,88 +154,97 @@ async function fetchPositions(
   const positions: Position[] = positionIds.map((id, index) => ({
     positionId: id,
     owner: owners[index],
+    network,
   }))
 
-  positionIds.length = 0;
-  owners.length = 0;
+  positionIds.length = 0
+  owners.length = 0
 
   return positions
 }
 
-export async function liquidationChecker() {
-  if (!process.env.MARKET || !markets[process.env.MARKET]) {
-    log(chalk.bold.red('MARKET must be provided'))
-    return
-  }
+async function fetchAllPositions(
+  market: MarketConfig
+): Promise<Partial<Record<Networks, Position[]>>> {
+  let allPositions: Partial<Record<Networks, Position[]>> = {}
 
-  if (!process.env.RPC_URLS) {
-    log(chalk.bold.red('At least one RPC_URLS must be provided'))
-    return
-  }
+  for (const network of Object.keys(market.networks) as Networks[]) {
+    const marketNetwork = market.networks[network]
+    if (!marketNetwork) continue
 
-  if (taskRunning) {
-    log(chalk.bold.red('Task is already running. Skipping this run...'))
-    return
-  }
+    const marketAddress = marketNetwork.address
+    const positionsPerRun = marketNetwork.positions_per_run
+    const totalPositions = await redis.zcard(`position_index:${network}:${marketAddress}`)
+    console.log('Total positions:', totalPositions)
+    console.log('Network:', network)
 
-  taskRunning = true
-  log(chalk.bold.blue('Cron job started at:', new Date().toLocaleString()))
-  const startTime = Date.now()
-
-  const marketAddress = markets[process.env.MARKET]
-  log(
-    'Liquidation Checker module is running for market:',
-    process.env.MARKET,
-    'with address:',
-    marketAddress
-  )
-
-  try {
-    const totalPositions = await redis.zcard(`position_index:${marketAddress}`)
-
-    const currentIndexKey = `current-index:${marketAddress}`
+    const currentIndexKey = `current-index:${network}:${marketAddress}`
     let currentIndexValue = await redis.get(currentIndexKey)
     let currentIndex = currentIndexValue ? parseInt(currentIndexValue) : 0
 
-    let endIndex = currentIndex + POSITIONS_PER_RUN
+    let endIndex = currentIndex + positionsPerRun
 
     let positions: Position[] = []
 
     if (endIndex >= totalPositions) {
       endIndex = totalPositions - 1
-      const positionsLeft = POSITIONS_PER_RUN - (endIndex - currentIndex)
+      const positionsLeft = positionsPerRun - (endIndex - currentIndex)
 
-      positions = await fetchPositions(marketAddress, currentIndex, endIndex)
+      positions = await fetchPositionsByNetwork(marketAddress, network, currentIndex, endIndex)
 
-      log('Processing positions from', currentIndex, 'to', endIndex)
+      log('Processing positions from', currentIndex, 'to', endIndex, 'for network', network)
 
       currentIndex = 0
       endIndex = currentIndex + positionsLeft
 
-      positions = positions.concat(await fetchPositions(marketAddress, currentIndex, endIndex))
+      positions = positions.concat(
+        await fetchPositionsByNetwork(marketAddress, network, currentIndex, endIndex)
+      )
 
-      log('Processing remaining positions from', currentIndex, 'to', endIndex)
+      log(
+        'Processing remaining positions from',
+        currentIndex,
+        'to',
+        endIndex,
+        'for network',
+        network
+      )
     } else {
-      positions = await fetchPositions(marketAddress, currentIndex, endIndex)
-      log('Processing positions from', currentIndex, 'to', endIndex)
+      positions = await fetchPositionsByNetwork(marketAddress, network, currentIndex, endIndex)
+      log('Processing positions from', currentIndex, 'to', endIndex, 'for network', network)
     }
-
-    await distributeWorkToWorkers(marketAddress, positions)
 
     // update the current index
     currentIndex = endIndex + 1
     await redis.set(currentIndexKey, currentIndex)
 
-    log(
-      chalk.bgGreen(
-        'All positions scanned successfully! time taken: ',
-        Date.now() - startTime,
-        'ms'
-      )
-    )
-  } catch (error) {
-    console.error('Error during the cron job:', error)
-  } finally {
-    taskRunning = false
+    allPositions[network] = positions
   }
+
+  console.log('All positions:', allPositions.movement?.length)
+  return allPositions
+}
+
+export async function liquidationChecker() {
+  if (!process.env.MARKET || !config[process.env.MARKET]) {
+    log(chalk.bold.red('MARKET must be provided'))
+    return
+  }
+
+  log(chalk.bold.blue('Cron job started at:', new Date().toLocaleString()))
+  const startTime = Date.now()
+
+  const market = config[process.env.MARKET]
+  log('Liquidation Checker module is running for market:', process.env.MARKET)
+
+  const positions = await fetchAllPositions(market)
+  await distributeWorkToWorkers(market, positions)
+
+  log(
+    chalk.bgGreen(
+      'All positions scanned successfully! time taken: ',
+      Date.now() - startTime,
+      'ms'
+    )
+  )
 }
--- packages/liquidation-checker/src/worker.ts
+++ packages/liquidation-checker/src/worker.ts
@@ -1,73 +1,92 @@
 import { parentPort, workerData } from 'worker_threads'
 import { ethers } from 'ethers'
-import { multicall2_address, olv_state_address } from './constants'
+import { LiquidatableResult, MulticallResult, Networks, Position } from './constants'
 import market_state_abi from './abis/market_state_abi.json'
 import multicall2_abi from './abis/multicall2_abi.json'
 import chalk from 'chalk'
 import redis from './redisHandler'
-
-interface Position {
-  positionId: string
-  owner: string
-}
-
-interface MulticallResult {
-  blockNumber: number
-  returnData: string[]
-}
-
-interface LiquidatableResult {
-  position: Position
-  isLiquidatable: boolean
-}
+import { MarketConfig, networkConfig } from './config'
+import { selectRpc } from './rpcHandler'
 
 const checkLiquidations = async (
-  positions: Position[],
-  marketAddress: string,
-  batchSize: number,
-  rpcUrl: string
+  market: MarketConfig,
+  positions: Partial<Record<Networks, Position[]>>
 ) => {
-  const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
-  const ovlMarketStateContract = new ethers.Contract(olv_state_address, market_state_abi, provider)
-  const multicall2Contract = new ethers.Contract(multicall2_address, multicall2_abi, provider)
-
-  const calls = positions.map((position) => ({
-    target: ovlMarketStateContract.address,
-    callData: ovlMarketStateContract.interface.encodeFunctionData('liquidatable', [
-      marketAddress,
-      position.owner,
-      parseInt(position.positionId),
-    ]),
-  }))
+  // const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
+  // const ovlMarketStateContract = new ethers.Contract(olv_state_address, market_state_abi, provider)
+  // const multicall2Contract = new ethers.Contract(multicall2_address, multicall2_abi, provider)
 
   const batchPromises: Promise<LiquidatableResult[]>[] = []
 
-  for (let i = 0; i < positions.length; i += batchSize) {
-    const batchPositions = positions.slice(i, i + batchSize)
+  for (const network of Object.keys(positions) as Networks[]) {
+    const marketNetwork = market.networks[network]
+    if (!marketNetwork) {
+      continue
+    }
 
-    // Add the multicall batch promise to the array
-    batchPromises.push(
-      multicall2Contract
-        .aggregate(calls.slice(i, i + batchSize))
-        .then((result: MulticallResult) => {
-          return result.returnData
-            .map((data, index) => {
-              const isLiquidatable = ovlMarketStateContract.interface.decodeFunctionResult(
-                'liquidatable',
-                data
-              )[0]
-              return {
-                position: batchPositions[index],
-                isLiquidatable: isLiquidatable,
-              }
-            })
-            .filter((result: LiquidatableResult) => result.isLiquidatable)
-        })
-        .catch((error: Error) => {
-          console.error(chalk.bold.red(`Error processing batch from ${i} to ${i + batchSize}:`), error)
-          return []
-        })
+    const calls: { target: string; callData: string }[] = []
+    const {
+      rpcUrls,
+      multicall2_address,
+      ovl_state_address,
+      multicall_batch_size,
+      rpc_first_probability,
+    } = networkConfig[network]
+
+    const rpcUrl = await selectRpc(rpcUrls, rpc_first_probability)
+    const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
+    const ovlMarketStateContract = new ethers.Contract(
+      ovl_state_address,
+      market_state_abi,
+      provider
     )
+    const multicall2Contract = new ethers.Contract(multicall2_address, multicall2_abi, provider)
+
+    console.log(`Checking liquidations for ${network} network`)
+    console.log('batch size:', multicall_batch_size)
+
+    for (const position of positions[network] as Position[]) {
+      calls.push({
+        target: ovlMarketStateContract.address,
+        callData: ovlMarketStateContract.interface.encodeFunctionData('liquidatable', [
+          marketNetwork.address,
+          position.owner,
+          parseInt(position.positionId),
+        ]),
+      })
+    }
+
+    const batchSize = multicall_batch_size
+
+    for (let i = 0; i < calls.length; i += batchSize) {
+      batchPromises.push(
+        multicall2Contract
+          .aggregate(calls.slice(i, i + batchSize))
+          .then((result: MulticallResult) => {
+            return result.returnData
+              .map((data, index) => {
+                const isLiquidatable = ovlMarketStateContract.interface.decodeFunctionResult(
+                  'liquidatable',
+                  data
+                )[0]
+                return {
+                  position: (positions[network] as Position[]).slice(i, i + batchSize)[index],
+                  isLiquidatable: isLiquidatable,
+                  network,
+                }
+              })
+              .filter((result: LiquidatableResult) => result.isLiquidatable)
+          })
+          .catch((error: Error) => {
+            console.error(
+              chalk.bold.red(
+                `Error processing batch from ${i} to ${i + batchSize} for ${network} network. RpcUrl: ${rpcUrl}`
+              )
+            )
+            return []
+          })
+      )
+    }
   }
 
   const results = await Promise.allSettled(batchPromises)
@@ -76,16 +95,23 @@ const checkLiquidations = async (
     .flatMap((result) => (result as PromiseFulfilledResult<LiquidatableResult[]>).value)
 
   for (const result of liquidatableResults) {
-    const { positionId, owner } = result.position
+    const marketNetwork = market.networks[result.network]
+    if (!marketNetwork) continue
 
-    const isNew = await redis.sadd('unique_positions', `${marketAddress}:${positionId}`)
+    const { positionId, owner } = result.position
+    const marketAddress = marketNetwork.address
+
+    const isNew = await redis.sadd(
+      'unique_positions',
+      `${result.network}:${marketAddress}:${positionId}`
+    )
     if (isNew) {
       console.log(
         `${chalk.green('Liquidatable position found => ')} ${chalk.yellow(
           'positionId:'
         )} ${positionId} ${chalk.yellow('owner:')} ${owner} ${chalk.yellow(
           'market:'
-        )} ${marketAddress}`
+        )} ${marketAddress} ${chalk.yellow('network:')} ${result.network}`
       )
       await redis.lpush(
         'liquidatable_positions',
@@ -93,6 +119,7 @@ const checkLiquidations = async (
           positionId,
           owner,
           marketAddress,
+          network: result.network,
         })
       )
     } else {
@@ -104,12 +131,7 @@ const checkLiquidations = async (
   return liquidatableResults.length
 }
 
-checkLiquidations(
-  workerData.positions,
-  workerData.marketAddress,
-  workerData.batchSize,
-  workerData.rpcUrl
-)
+checkLiquidations(workerData.market, workerData.positions)
   .then((results) => {
     if (parentPort) parentPort.postMessage(results)
   })
