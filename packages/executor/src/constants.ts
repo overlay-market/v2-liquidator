eded951ee2a1a29ce5128f038204329df4ef2f26
djaciel <dj_roses@hotmail.com>
2024-08-11 19:51:43 -0600

    add constants file to executor

---
+++ packages/executor/src/constants.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export interface Position {
+  positionId: string
+  owner: string
+  marketAddress: string
+  network: Networks
+}
+
+export enum Networks {
+  ARBITRUM = "arbitrum",
+  BERACHAIN = "berachain",
+  MOVEMENT = "movement",
+}
+
+export interface NetworksConfig {
+  rpc_url: string;
+  useOldMarketAbi?: boolean;
+}
+
+export const networksConfig: Record<Networks, NetworksConfig> = {
+  [Networks.ARBITRUM]: {
+    rpc_url: process.env.ARBITRUM_RPC_URLS?.split(',')[0] || 'https://arbitrum-sepolia-rpc.publicnode.com',
+    useOldMarketAbi: true,
+  },
+  [Networks.BERACHAIN]: {
+    rpc_url: process.env.BERACHAIN_RPC_URLS?.split(',')[0] || 'https://bartio.rpc.berachain.com/',
+  },
+  [Networks.MOVEMENT]: {
+    rpc_url: process.env.MOVEMENT_RPC_URLS?.split(',')[0] || 'https://mevm.devnet.imola.movementlabs.xyz',
+  },
+}
