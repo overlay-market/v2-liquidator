fbc6aa1958038f7dbec421fca9acc4846fd43d36
djaciel <dj_roses@hotmail.com>
2025-01-08 10:59:39 -0600

    add redis handler and update healthcheck endpoint to return executor addresses

--- packages/health-checker/src/server.ts
+++ packages/health-checker/src/server.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import redis from './redisHandler';
 
 const app = express();
 const port = process.env.PORT || 2025;
 
-// Endpoint healthcheck
-app.get('/healthcheck', (req, res) => {
-  res.status(200).send('Server is alive');
+async function getExecutors() {
+  const keys = await redis.keys(`total_liquidated_positions_by_executor:${"berachain"}:*`)
+  const executorAddresses = keys.map((key) => key.split(':')[2])
+  return executorAddresses
+}
+
+// Endpoint healthcheck also returns the executors addresses
+app.get('/healthcheck', async (req, res) => {
+  res.status(200).send({
+    status: 'ok',
+    executors: await getExecutors()
+  });
 });
 
 // Start the server
