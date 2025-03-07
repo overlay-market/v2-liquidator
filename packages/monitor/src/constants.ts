db934f2a1c509532d47f1bcbd30e22d9013593a8
djaciel <dj_roses@hotmail.com>
2025-03-06 19:41:36 -0600

    update OVL token address

--- packages/monitor/src/constants.ts
+++ packages/monitor/src/constants.ts
@@ -12,7 +12,7 @@ export const networkConfig: Record<Networks, NetworkConfig> = {
   [Networks.BERACHAIN]: {
     rpcUrl: 'https://bartio.rpc.berachain.com/',
     apiUrl: 'https://api.overlay.market/bartio-charts/v1/charts/markets',
-    ov_token_address: '0x97576e088f0d05EF68cac2EEc63d017FE90952a0',
+    ov_token_address: '0xaeD2f0c7AaCfE2B59Cc70964833EA4C28C2CdbDB',
   },
 }
 
